4f0c7c15209f3348030e2ab7e5eed12235890e3d
icaro <4553385243@estudante.sed.sc.gov.br>
2024-06-19 21:31:08 -0300

    primeiros exercicios de java script-pra arrumar

---
+++ e.js
@@ -0,0 +1,51 @@
+//ex 1
+let nome=("joao")
+
+console.log(nome)
+
+//ex 2
+let a=5; b=10; soma=(a+b)
+
+console.log(soma)
+
+//ex 3
+
+let primeironome="josefa" ;segundonome=" silva";nome=primeironome+segundonome
+
+console.log(nome)
+//ex 4
+let pessoa={nome:"icaro",
+    idade:14,
+    endereço:"blumenau",
+    telefone:"4002-8022"
+}
+console.log(pessoa)
+//ex 5
+let vetor=["maça","banana","laranja"]
+
+vetor.push("uva")
+
+console.log (vetor)
+
+vetor.clear
+//ex 6
+vetor=[1,2,3,4,5]
+
+console.log(vetor[0],vetor[4])
+
+let cores=["vermelho","verde","azul"]
+for(i=0; i<cores.length; i++){
+    console.log(cores[i])
+}
+//ex 7
+let idade=14
+
+if(idade<18){
+    console.log("menor de idade")
+}
+else if(idade>=18)
+console.log("maior de idade")
+
+
+//ex 8
+
